457e1d3ab0198ffdc65a34b540fa56ae742044b2
Vinod <vinu.pankajakshan@gmail.com>
2015-02-28 04:32:33 +0000

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -6,8 +6,8 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
-# Create your tests here.
-class SmokeTest(TestCase):
+
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -27,13 +27,35 @@ class SmokeTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_home_page_displays_all_lists(self):
+        Item.objects.create(text='Item arry 1')
+        Item.objects.create(text='Item arry 2')
+        
+        request = HttpRequest()
+        response = home_page(request)
+        
+        self.assertIn('Item arry 1', response.content.decode())
+        self.assertIn('Item arry 2', response.content.decode())
+        
+    def test_home_page_only_saves_item_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
